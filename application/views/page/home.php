ba4a9d5f9cf83adac37afeb63ab3d3719511052a
Frederick Vuong <vuong.fred@gmail.com>
2011-12-13 17:30:38 -0700

    more styling and typography done, added some content to home page

--- application/views/page/home.php
+++ application/views/page/home.php
@@ -1,10 +1,29 @@
-<div class='medium_grey'>Hi, I'm <span class='big_grey'>Fred</span> and I'm a <span class='big_grey'>Web Developer</span></div>
+<div class='medium_grey1'>
+	Hi, I'm <span class='big_grey1'>Fred.</span>
+</div>
+<div class='medium_grey2'>
+	I develop <span class='big_grey2'>clean</span> &amp; <span class='big_grey2'>simple</span><br/>
+	<span class='big_grey2'>websites</span> &amp; <span class='big_grey2'>web applications.</span>
+</div>
+<div id='middle'></div>
 <div class='bottom_box'>
-	<div class='box_title'>What I do</div>
+	<div class='bottom_box_title'>Who am I</div>
+	<div class='bottom_box_content'>
+		I'm a recent graduate from the University of Alberta who enjoys building functional, creative, and usability-oriented web sites 
+		and applications by implementing the latest web standards and technologies.
+	</div>
 </div>
 <div class='bottom_box'>
-	<div class='box_title'>What I do</div>
+	<div class='bottom_box_title'>What I do</div>
+	<div class='bottom_box_content'>
+		I develop web sites and web applications with the use of various programming languages and technologies.
+		Whether the project is a simple static website or a dynamic database driven application, I have the skills and 
+		experience to make it happen.
+	</div>
 </div>
-<div class='bottom_box'>
-	<div class='box_title'>What I do</div>
+<div class='bottom_box bottom_box_last'>
+	<div class='bottom_box_title'>Get in touch</div>
+	<div class='bottom_box_content'>
+		vuong.fred@gmail.com
+	</div>
 </div>
